5226fee03706bcea5a4948d205467ade2adbb41c
Aiden <aidenwhite27@gmail.com>
2020-04-18 00:42:01 -0600

    Initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer == 9)
+        {
+            if (collider.GetComponent<Rigidbody2D>().velocity.y <= 0f)
+            {
+                Destroy(this.gameObject);
+            }           
+        }
+    }
+}
